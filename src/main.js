f0359b012e2bce3301c7c0e80c4890d7bb932b23
edgar6bf <edgar6bf@gmail.com>
2023-07-22 17:56:37 +0900

    feat: js home 투명도 조절 구현

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ const header = document.querySelector(".header");
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
     // 스크롤되는 Y 좌표가 headerHeight 보다 크다면 다른 스타일링!
-    console.log(window.scrollY);
     if (window.scrollY > headerHeight) {
         header.classList.add('header--dark');
     } else {
         header.classList.remove('header--dark');
     }
 });
+
+// Home Section을 아래로 스크롤시 투명하게 처리
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - (window.scrollY / homeHeight);
+    console.log(home.style.opacity);
+});
+
